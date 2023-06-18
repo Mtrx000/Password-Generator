85e27723bfbbef32e0cad566c46bfcbcad842dcf
Mtrx000 <mtrxsrv@gmail.com>
2023-06-18 09:52:07 +0200

    update

---
+++ Password generator/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 92)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(91, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Special Chars"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 115)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(84, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "UpperCases"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(136, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Longueur du mot de passe:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(151, 57)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(57, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(225, 139)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 36)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Generate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(62, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(203, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Simple Password generator"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(11, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "*"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 195)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Password Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Password generator/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Reflection.Emit
+Imports System.Text
+Imports System.Windows.Forms
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Button
+
+Public Class Form1
+    '                                                               Allink Project
+    '                                                                          By Mtrx000
+    '                                                               CONTACT ME ON DISCORD TO USE THIS CODE
+    '                                                         Discord: r3vil_
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim passwordLength As Integer = Integer.Parse(TextBox1.Text)
+        Dim includeSpecialChars As Boolean = CheckBox1.Checked
+        Dim includeUpperCase As Boolean = CheckBox2.Checked
+
+        Dim password As String = GeneratePassword(passwordLength, includeSpecialChars, includeUpperCase)
+        Label3.Text = password
+    End Sub
+
+    '                                                               Allink Project
+    '                                                                          By Mtrx000
+    '                                                               CONTACT ME ON DISCORD TO USE THIS CODE
+    '                                                         Discord: r3vil_
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Clipboard.SetText(Label1.Text)
+        MessageBox.Show("Copied To Clipboard !")
+    End Sub
+
+    '                                                               Allink Project
+    '                                                                          By Mtrx000
+    '                                                               CONTACT ME ON DISCORD TO USE THIS CODE
+    '                                                         Discord: r3vil_
+
+    Private Function GeneratePassword(length As Integer, includeSpecialChars As Boolean, includeUpperCase As Boolean) As String
+        Dim chars As String = "abcdefghijklmnopqrstuvwxyz"
+        Dim rnd As New Random()
+        Dim password As New StringBuilder(length)
+
+        If includeUpperCase Then
+            chars &= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        End If
+
+        If includeSpecialChars Then
+            chars &= "#@%$"
+        End If
+
+        For i As Integer = 1 To length
+            Dim index As Integer = rnd.Next(0, chars.Length)
+            password.Append(chars(index))
+        Next
+
+        Return password.ToString()
+    End Function
+End Class
+
+
+'                                                               Allink Project
+'                                                                          By Mtrx000
+'                                                               CONTACT ME ON DISCORD TO USE THIS CODE
+'                                                         Discord: r3vil_
